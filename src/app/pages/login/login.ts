b9be77466e9f2589457a19c015d19b87b3e6b380
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2025-12-10 19:49:07 -0400

    Adicionando folder app

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+
+}
